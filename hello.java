8dc3da5e7971dd3d9cff9f08da21583259d2af18
mhd <776741417@qq.com>
2023-09-05 13:56:44 +0800

    new dev

--- hello.java
+++ hello.java
@@ -2,9 +2,11 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("hello");
 		System.out.println("helloWorld");
+		System.out.println("hellochina");
+		System.out.println("hellodev");
 	}
 
 
 }
-public class hello; hot-fix
+
 
